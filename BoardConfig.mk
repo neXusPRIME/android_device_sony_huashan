cc84e3c900b352e24e5b4b803ee213b6cb9af8be
neXusPRIME <yasir.elec@gmail.com>
2014-07-17 18:32:23 +0500

    Update CM tree with all fixes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,16 +23,15 @@ include device/sony/qcom-common/BoardConfigCommon.mk
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := C5302,C5303,C5306,huashan
-#TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/sony/huashan/releasetools/ota_from_target_files
-
-# Add support for all recoveries
-USE_SET_METADATA := true
 
 TARGET_SPECIFIC_HEADER_PATH += device/sony/huashan/include
 
 # Kernel properties
-TARGET_KERNEL_SOURCE := kernel/sony/huashan
+TARGET_KERNEL_SOURCE := kernel/sony/msm8960t
 TARGET_KERNEL_CONFIG := viskan_huashan_defconfig
+
+# Allow moving app2SD
+TARGET_EXTERNAL_APPS = sdcard1
 
 # Platform
 TARGET_BOOTLOADER_BOARD_NAME := MSM8960
@@ -41,10 +40,10 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno320
 BOARD_VENDOR_PLATFORM := viskan
 
 # Architecture
-TARGET_CPU_VARIANT := krait
 TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_CPU_VARIANT := krait
 
-# Flags
+# Architecture flags
 TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
@@ -84,10 +83,6 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 # GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
 TARGET_NO_RPC := true
-
-# FM radio
-BOARD_USES_STE_FMRADIO := true
-COMMON_GLOBAL_CFLAGS += -DSTE_FM
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
@@ -130,8 +125,8 @@ TARGET_USES_QCOM_BSP := true
 BOARD_USES_ALSA_AUDIO := true
 BOARD_USES_LEGACY_ALSA_AUDIO := true
 TARGET_USES_QCOM_COMPRESSED_AUDIO := true
-TARGET_USES_QCOM_MM_AUDIO := true
 TARGET_LS_USE_ALS_NODE := true
+BOARD_HAVE_NEW_QCOM_CSDCLIENT := true
 
 # QCOM enhanced A/V
 TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
@@ -147,24 +142,32 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
 BOARD_SEPOLICY_DIRS += \
     device/sony/huashan/sepolicy
 
-BOARD_SEPOLICY_UNION := \
-       app.te \
-       bluetooth.te \
-       device.te \
-       domain.te \
-       drmserver.te \
-       file.te \
-       file_contexts \
-       hci_init.te \
-       init_shell.te \
-       keystore.te \
-       mediaserver.te \
-       kickstart.te \
-       netd.te \
-       rild.te \
-       surfaceflinger.te \
-       system.te \
-       ueventd.te \
-       vold.te \
-       wpa.te
+BOARD_SEPOLICY_UNION += \
+    file_contexts \
+    property_contexts \
+    te_macros \
+    bluetooth_loader.te \
+    bridge.te \
+    camera.te \
+    device.te \
+    dhcp.te \
+    domain.te \
+    drmserver.te \
+    file.te \
+    kickstart.te \
+    init.te \
+    mac_update.te \
+    mediaserver.te \
+    mpdecision.te \
+    netmgrd.te \
+    qmux.te \
+    rild.te \
+    rmt.te \
+    surfaceflinger.te \
+    system.te \
+    tee.te \
+    thermald.te \
+    ueventd.te \
+    vold.te \
+    wpa_supplicant.te
 
