9eabd24e8b36ca8393871cc3bc3b958898d15dbc
neXusPRIME <yasir.elec@gmail.com>
2014-07-29 12:03:36 +0500

    Add releasetools patch for LBL and fix Torch path

--- full_huashan.mk
+++ full_huashan.mk
@@ -28,7 +28,7 @@ TARGET_SCREEN_WIDTH := 720
 
 # Torch
 PRODUCT_PACKAGES := \
-    Lightbulb
+    Torch
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
