13dbb39366591c4f9c4f754b13b7cfd348d54771
neXusPRIME <yasir.elec@gmail.com>
2014-07-29 11:22:13 +0500

    Fix patches and build path

---
+++ cm.mk
@@ -0,0 +1,16 @@
+$(call inherit-product, device/sony/huashan/full_huashan.mk)
+
+# Inherit CM common GSM stuff.
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit CM common Phone stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Build fingerprints
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5303 BUILD_FINGERPRINT=Sony/C5303/C5303:4.3/12.1.A.1.205/Mvt_nw:user/release-keys PRIVATE_BUILD_DESC="C5303-user 4.3 JB-MR2-VISKAN-140516-0635 235 test-keys"
+
+PRODUCT_NAME := cm_huashan
+PRODUCT_DEVICE := huashan
