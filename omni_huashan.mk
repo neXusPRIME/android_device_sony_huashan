210fdc1b7889924e67717bd6874dda964fe449ff
neXusPRIME <yasir.elec@gmail.com>
2014-10-06 16:18:55 +0500

    yet more fixes

--- omni_huashan.mk
+++ omni_huashan.mk
@@ -17,14 +17,8 @@
 # Sample: This is where we'd set a backup provider if we had one
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 720
-
 # Inherit GSM configuration
 $(call inherit-product, vendor/omni/config/gsm.mk)
-
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
@@ -40,6 +34,3 @@ PRODUCT_MANUFACTURER := Sony
 
 # Build fingerprints
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5303 BUILD_FINGERPRINT=Sony/C5303/C5303:4.3/12.1.A.1.207/MPt_nw:user/release-keys PRIVATE_BUILD_DESC="C5303-user 4.3 JB-MR2-VISKAN-140708-0812 262 test-keys"
-
-# Common Sony Resources
-$(call inherit-product, device/sony/lbcommon/resources.mk)
